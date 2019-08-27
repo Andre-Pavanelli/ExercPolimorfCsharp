d27ca1420de7529dd3ed1f61eb71feb19e6865ef
Andre Pavanelli <andpavanelli@gmail.com>
2019-08-27 20:45:49 -0300

    Add project files.

---
+++ ExercicioPolProduct/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioPolProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
